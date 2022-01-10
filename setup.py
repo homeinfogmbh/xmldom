4be260da75f19b60413133f163e61fc0d8367740
Richard Neumann <r.neumann@homeinfo.de>
2022-01-10 10:35:19 +0100

    MIgrate to setuptools

--- setup.py
+++ setup.py
@@ -1,14 +1,19 @@
 #! /usr/bin/env python3
 
-from distutils.core import setup
+from setuptools import setup
 
 setup(
     name='xmldom',
-    version='latest',
+    use_scm_version={
+        "local_scheme": "node-and-timestamp"
+    },
+    setup_requires=['setuptools_scm'],
     author='HOMEINFO - Digitale Informationssysteme GmbH',
     author_email='<info at homeinfo dot de>',
     maintainer='Richard Neumann',
     maintainer_email='<r dot neumann at homeinfo period de>',
+    install_requires=['pyxb'],
     py_modules=['xmldom'],
     license='GPLv3',
-    description='An XML DOM library using PyXB.')
+    description='An XML DOM library using PyXB.'
+)
